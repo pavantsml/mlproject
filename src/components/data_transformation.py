002b5b3e02fcf481198de7670a7faa91d84a0100
pavantsml <pavankumarshivasali@gmail.com>
2024-11-20 09:07:51 +0530

    t/COMMIT_EDITMSG [unix] (09:07 20/11/2024) 24,1 Bot

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -16,7 +16,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
@@ -54,7 +54,7 @@ class DataTransformation:
                 ]
 
             )
-
+            
             logging.info(f"Categorical columns: {categorical_columns}")
             logging.info(f"Numerical columns: {numerical_columns}")
 
